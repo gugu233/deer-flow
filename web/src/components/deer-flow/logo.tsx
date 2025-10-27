42533644affc5ba52df4c26e10aee62a49f3fc2e
guxiaoxiao <guxiaoxiao@bytedance.com>
2025-10-27 20:19:13 +0800

    feat: writer and mv unused

--- web/src/components/deer-flow/logo.tsx
+++ web/src/components/deer-flow/logo.tsx
@@ -9,7 +9,7 @@ export function Logo() {
       className="opacity-70 transition-opacity duration-300 hover:opacity-100"
       href="/"
     >
-      🦌 DeerFlow
+      🦌 一站式治理平台
     </Link>
   );
 }
